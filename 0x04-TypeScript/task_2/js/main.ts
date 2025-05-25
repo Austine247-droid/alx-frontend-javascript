720d74a7240f0721f57ced0e0c959e9a2b0be6d4
Austine247-droid <ugberaeseoa@gmail.com>
2025-05-25 20:27:32 +0000

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -5,3 +5,75 @@ interface DirectorInterface {
   getCoffeeBreak(): string;
   workDirectorTasks(): string;
 }
+
+// Create the TeacherInterface interface with the 3 expected methods:
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+// Create a class Director that will implement DirectorInterface
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Directing tasks";
+  }
+}
+
+// Create a class Teacher that will implement TeacherInterface
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+// Create a function createEmployee that takes a parameter of type string or number
+const createEmployee = (salary: number | string): Director | Teacher => {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+};
+
+// Creating functions specific to employees
+const isDirector = (employee: Director | Teacher): employee is Director => {
+  return employee instanceof Director;
+};
+
+const executeWork = (employee: Director | Teacher): string => {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+};
+
+// Write a String literal type named Subjects allowing a variable to have the value Math or History only. Write a function named teachClass:
+// it takes todayClass as an argument
+// it will return the string Teaching Math if todayClass is Math
+// it will return the string Teaching History if todayClass is History
+
+type Subjects = "Math" | "History";
+
+const teachClass = (todayClass: Subjects): string => {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  }
+  return "Teaching History";
+};
